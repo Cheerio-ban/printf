0cf8090434f232b52624ed6bede43cd28f0a58b3
Esther Anuoluwapo Adejola <adejolanu@gmail.com>
2022-07-10 14:44:41 -0700

    Added the function manager

---
+++ function_manager.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdarg.h>
+/**
+ * _switch - evaluates cases
+ * @c: character to find
+ * @arg: va_list type
+ * Return: count of printed characters
+ */
+int _switch(char c, va_list arg)
+{
+	int cont = 0;
+
+	switch (c)
+	{
+		case 'c':
+			cont += print_unsign(arg, 2);
+			break;
+		case 'd':
+		case 'i':
+			cont += print_sign(arg, 10);
+			break;
+		case 's':
+			cont += print_string(arg);
+			break;
+		default:
+			cont = -1;
+	}
+	return (cont);
+}
