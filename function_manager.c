a38dc972c709fa0403a03037a0dccd4590a12017
Esther Anuoluwapo Adejola <adejolanu@gmail.com>
2022-07-10 14:49:16 -0700

    Added the function manager

--- function_manager.c
+++ function_manager.c
@@ -27,3 +27,19 @@ int _switch(char c, va_list arg)
 	}
 	return (cont);
 }
+
+/**
+ * function_manager - calls other functions
+ * @c: character to find
+ * @arg: va_list type
+ * Return: count of printed characters
+ */
+int function_manager(char c, va_list arg)
+{
+	int cont = 0;
+
+	cont = _switch(c, arg);
+	if (c == 'p')
+		cont = print_ptr(arg);
+	return (cont);
+}
