adab45e3cf85e3ad10961c11607f6afdac7a9cc1
Esther Anuoluwapo Adejola <adejolanu@gmail.com>
2022-07-12 08:23:14 -0700

    Updated the printf project files

--- _printf.c
+++ _printf.c
@@ -1,117 +1,111 @@
 #include "main.h"
-#include <stdio.h>
-#include <stddef.h>
-#include <string.h>
-#include <stdarg.h>
-
 /**
- * _printf - function like printf
- * @format: string format to be printed
- *
- * Return: number of characters printed
+ *_printf - implements some functions of printf
+ *@format: const char pointer
+
+ *Return: num of characteres printed
  */
 int _printf(const char *format, ...)
 {
-	int d = 0, num;
-	unsigned int numm, m;
-	va_list ap;
-	char charr;
-	char *ptr;
+	const char *string;
+	int cont = 0;
+	va_list arg;
 
-	va_start(ap, format);
-	if ((*format == '\0') || (format == NULL))
-		return (d);
-	while (*format != '\0')
+	if (!format)
+		return (-1);
+
+	va_start(arg, format);
+	string = format;
+
+	cont = loop_format(arg, string);
+
+	va_end(arg);
+	return (cont);
+}
+/**
+ *loop_format - loop format
+ *@arg: va_list arg
+ *@string: pointer from format
+ *Description: This function make loop tp string pointer
+ *Return: num of characteres printed
+ */
+int loop_format(va_list arg, const char *string)
+{
+	int i = 0, flag = 0, cont_fm = 0, cont = 0, check_per = 0;
+
+	while (i < _strlen((char *)string) && *string != '\0')
 	{
-		if (*format == '%')
+		char aux = string[i];
+
+		if (aux == '%')
 		{
-			if (*(format + 1) == '%')
+			i++, flag++;
+			aux = string[i];
+			if (aux == '\0' && _strlen((char *)string) == 1)
+				return (-1);
+			if (aux == '\0')
+				return (cont);
+			if (aux == '%')
 			{
-				_putchar('%');
-				d++;
-				format += 2;
-			}
-			else if (*(format + 1) == 's')
+				flag++;
+			} else
 			{
-				ptr = va_arg(ap, char *);
-				while (*ptr != '\0')
+				cont_fm = function_manager(aux, arg);
+				if (cont_fm >= 0 && cont_fm != -1)
 				{
-					_putchar(*ptr);
-					d++;
-					ptr++;
-				}
-				format += 2;
-			}
-			else if (*(format + 1) == 'c')
-			{
-				charr = va_arg(ap, int);
-				_putchar(charr);
-				d++;
-				format += 2;
-			}
-			else if ((*(format + 1) == 'd') || (
-					*(format  + 1) == 'i'))
-			{
-				num = va_arg(ap, int);
-				d += print_number(num);
-				format += 2;
-			}
-			else if (*(format + 1) == 'u')
-			{
-				numm = va_arg(ap, int);
-				d += uprint_number(numm);
-				format += 2;
-			}
-			else if (*(format + 1) == 'p')
-			{
-				m = va_arg(ap, int);
-				ptr = getp(m);
-				while (*ptr != '\0')
+					i++;
+					aux = string[i];
+					if (aux == '%')
+						flag--;
+					cont = cont + cont_fm;
+				} else if (cont_fm == -1 && aux != '\n')
 				{
-					_putchar(*ptr);
-					ptr++;
+					cont += _putchar('%');
 				}
-				d++;
-				format += 2;
-			}
-			else if (*(format + 1) == 'b')
-			{
-				numm = va_arg(ap, int);
-				numm = cbinary(numm, 0);
-				d += uprint_number(numm);
-				format += 2;
-			}
-			else if (*(format + 1) == 'X')
-			{
-				numm = va_arg(ap, int);
-				d += chex(numm);
-				format += 2;
-			}
-			else if (*(format + 1) == 'x')
-			{
-				numm = va_arg(ap, int);
-				d += low_hex(numm);
-				format += 2;
-			}
-			else if (*(format + 1) == 'o')
-			{
-				numm = va_arg(ap, int);
-				d += coctal(numm);
-				format += 2;
-			}
-			else
-			{
-				_putchar(*format);
-				d++;
-				format++;
 			}
 		}
-		else
-		{
-			_putchar(*format);
-			d++;
-			format++;
-		}
+		check_per = check_percent(&flag, aux);
+		cont += check_per;
+		if (check_per == 0 && aux != '\0' && aux != '%')
+			cont += _putchar(aux), i++;
+		check_per = 0;
 	}
-	return (d);
+	return (cont);
+}
+/**
+ * check_percent - call function manager
+ *@flag: value by reference
+ *@aux: character
+ *Description: This function print % pear
+ *Return: 1 if % is printed
+ */
+int check_percent(int *flag, char aux)
+{
+	int tmp_flag;
+	int cont = 0;
+
+	tmp_flag = *flag;
+	if (tmp_flag == 2 && aux == '%')
+	{
+		_putchar('%');
+		tmp_flag = 0;
+		cont = 1;
+	}
+	return (cont);
+}
+
+/**
+ * call_function_manager - call function manager
+ *@aux: character parameter
+ *@arg: va_list arg
+ *Description: This function call function manager
+ *Return: num of characteres printed
+ */
+
+int call_function_manager(char aux, va_list arg)
+{
+	int cont = 0;
+
+	cont = function_manager(aux, arg);
+	return (cont);
 }
--- main.h
+++ main.h
@@ -1,17 +1,24 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
 int _putchar(char c);
+void _puts(char *str);
+int _strlen(char *s);
 int _printf(const char *format, ...);
-int print_number(int n);
-unsigned int cbinary(unsigned int n, unsigned int d);
-void rev_string(char *s);
-int chex(unsigned int n);
-int low_hex(unsigned  int n);
-int coctal(unsigned int n);
-int uprint_number(unsigned int n);
-char *pts(void * x);
-char *getp(unsigned int x);
-
+char *convert_to(char representation[], unsigned int num, int base);
+int function_manager(char c, va_list arg);
+int _switch(char c, va_list arg);
+int print_character(va_list arg);
+int print_sign(va_list arg, int base);
+int print_unsign(va_list arg, int base);
+int print_string(va_list arg);
+int print_ptr(va_list arg);
+int print_rot13(va_list arg);
+int print_rev(va_list arg);
+int print_base16_upper_lower(va_list arg, char *representation);
+int loop_format(va_list arg, const char *string);
+int call_function_manager(char aux, va_list arg);
+int check_percent(int *flag, char aux);
 
 #endif
